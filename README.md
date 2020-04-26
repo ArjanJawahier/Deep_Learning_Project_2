29ed7e615aeb498a4dfa6d659f44e2345e15ef1e
ArjanJawahier <arjanjawahier@hotmail.com>
2020-04-26 12:02:13 +0200

    Added LSGANLoss and Discriminator implementations and update the README

--- README.md
+++ README.md
@@ -25,11 +25,19 @@ The following tutorials and documents can be read to understand what to do.
 1) [PyTorch GAN Tutorial](https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html).
 2) [A lot of documentation and examples of GANs (highly recommended)](https://github.com/nashory/gans-awesome-applications)
 3) [Style Transfer paper](https://arxiv.org/pdf/1703.07511.pdf)
-4) [CycleGAN paper](https://arxiv.org/pdf/1703.10593.pdf) - We are implementing this
+4) [CycleGAN paper](https://arxiv.org/pdf/1703.10593.pdf) - We are implementing this.
 5) [ResNet paper](https://arxiv.org/pdf/1512.03385.pdf) - This is what our Generators will consist of.
+6) [PatchGAN paper](https://arxiv.org/pdf/1611.07004.pdf) - This is what our Disciminators will consist of.
+7) [LSGAN paper](https://arxiv.org/pdf/1611.04076.pdf) - Least Squares Loss function for the discriminator.
 
 ## Discriminator
 The discriminator coded in discriminator.py is trying to classify input images as either real or fake. When we want to transfer a style (e.g. Van Gogh) to another image (e.g. your house), the discriminator will compare the generated image of your house with the Van Gogh style to real Van Gogh images. The goal is for the discriminator to essentially randomly guess whether an image is real or fake (p = 0.5).
 
+We will be using PatchGANs for our discriminator models. These PatchGANs can determine whether each patch of 70x70 pixels is fake or real in a convolutional fashion. This causes the PatchGAN to be applicable to images of any size, while also having fewer parameters than e.g. a 128x128 pixel GAN disciminator. 
+
+The discriminator loss function will be MSELoss. We do this as this is recommended by the LSGAN paper. Essentially, using the least squares method instead of other loss functions will force the GANs to generate images that lie close to the decision boundary.
+
 ## Generator
 The generator coded in generator.py is trying to create fake images that look like the given style. We can give the generator an input image and an input style, such that the output image has positive semantic correlation with the given input image and the given input style. The goal is for the generator to generate images such that these cannot be discriminated from real images by the discriminator.
+
+The generator will consist of a few downsampling layers and upsampling layers, with a number of ResNet blocks in between. ResNet blocks allow deep architectures to become even deeper, as ResNet blocks combat the vanishing/exploding gradient problem. 
--- cyclegan.py
+++ cyclegan.py
@@ -1,4 +1,7 @@
-## CycleGAN remake
+## CycleGAN remake, we follow the structure of the PyTorch implementation
+## given by the authors of the CycleGAN paper.
+## PyTorch implementation: https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/blob/master/
+## CycleGAN paper: https://arxiv.org/pdf/1703.10593.pdf
 
 # Todo-list:
 # 1) Get dataset of normal items.
@@ -27,6 +30,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import PIL
+import itertools
 
 class Generator(nn.Module):
     """ResNet, but hardcode the layers (in contrast to the real CycleGAN).
@@ -35,8 +39,8 @@ class Generator(nn.Module):
 
     def __init__(self, opt):
         super(Generator, self).__init__()
-        self.n_blocks = 6       # Hardcoded for now
-        ngf = 64           # Harcoded, num filters in last conv layer
+        self.n_blocks = 6
+        ngf = 64           # Hardcoded, num filters in last conv layer
 
         # The ReflectionPad2d layer pads the input tensor. The value is 3 because we use a 7x7 conv layer
         self.model = [nn.ReflectionPad2d(3),
@@ -55,7 +59,7 @@ class Generator(nn.Module):
         # Add ResNet blocks
         mult = 2 ** n_downsampling_layers
         for i in range(self.n_blocks):
-            self.model += [ResnetBlock(ngf, mult)] # todo: understand these blocks! https://arxiv.org/pdf/1512.03385.pdf
+            self.model += [ResnetBlock(ngf, mult)] # see https://arxiv.org/pdf/1512.03385.pdf
 
         # Add upsampling layers
         for i in range(n_downsampling_layers):
@@ -73,8 +77,8 @@ class Generator(nn.Module):
 
         self.model = nn.Sequential(*self.model)
 
-    def forward(self, input):
-        self.model(input)
+    def forward(self, x):
+        self.model(x)
 
 
 class ResnetBlock(nn.Module):
@@ -92,8 +96,8 @@ class ResnetBlock(nn.Module):
                       nn.BatchNorm2d(ngf * mult)] 
         return nn.Sequential(*conv_block)
 
-    def forward(self, input):
-        """ResNet forward function: todo read paper and understand this! https://arxiv.org/pdf/1512.03385.pdf"""
+    def forward(self, x):
+        """ResNet forward function: https://arxiv.org/pdf/1512.03385.pdf"""
         out = x + self.conv_block(x)
         return out
 
@@ -101,13 +105,53 @@ class ResnetBlock(nn.Module):
 class Discriminator(nn.Module):
     """Implementing the PatchGAN used by the CycleGAN authors.""" 
     def __init__(self, opt):
-        pass
+        super(Discriminator, self).__init__()
+        ndf = 64
+        num_layers = 3
+        kw = 4
+        padw = 1
+        self.model = [nn.Conv2d(opt.input_nc, ndf, kernel_size=kw, stride=2, padding=padw),
+                      nn.LeakyReLU(0.2, True)]
+        nf_mult = 1
+        nf_mult_prev = 1
+        for i in range(1, num_layers):
+        	nf_mult_prev = nf_mult
+        	nf_mult = min(2 ** i, 8)
+        	self.model += [nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult, 
+        							 kernel_size=kw, stride=2, padding=padw, bias=False),
+        				   nn.BatchNorm2d(ndf * nf_mult),
+        				   nn.LeakyReLU(0.2, True)]
 
-    def forward(self, input):
-        pass
+        nf_mult_prev = nf_mult
+        nf_mult = min(2 ** num_layers, 8)
+        self.model += [nn.Conv2d(ndf * nf_mult_prev, ndf * nf_mult,
+        						 kernel_size=kw, stride=1, padding=padw, bias=False),
+        			   nn.BatchNorm2d(ndf * nf_mult),
+        			   nn.LeakyReLU(0.2, True),
+        			   nn.Conv2d(ndf * nf_mult, 1, kernel_size=kw, stride=1, padding=padw)] # 1 channel output
 
-    def backward(self):
-        pass
+        self.model = nn.Sequential(*self.model)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class LSGANLoss(nn.Module):
+	def __init__(self):
+		super(LSGANLoss, self).__init__()
+		self.loss = nn.MSELoss() # see LSGAN paper as to why
+		target_real_label = 1.0
+		target_fake_label = 0.0
+		self.register_buffer("real_label", torch.tensor(target_real_label))
+		self.register_buffer("fake_label", torch.tensor(target_fake_label))
+
+	def __call__(self, prediction, target_is_real):
+		if target_is_real:
+			target_tensor = self.real_label.expand_as(prediction)
+		else:
+			target_tensor = self.fake_label.expand_as(prediction)
+
+		return self.loss(prediction, target_tensor)
 
 
 class Options:
@@ -115,16 +159,30 @@ class Options:
     def __init__(self):
         self.input_nc = 3       # num channels, usually 3 (RGB)
         self.output_nc = 3      # num channels, usually 3 (RGB)
-
+        self.num_epochs = 5
+        self.lr = 0.0002		# Learning rate
+        self.beta1 = 0.5 		# beta1 parameter for the Adam optimizers
 
 class CycleGAN:
-    def __init__(self):
+    def __init__(self, is_train):
         self.opt = Options()        # Hardcoded options
-        self.G_A = Generator(self.opt)      # todo: args
-        self.G_B = Generator(self.opt)      # todo: args
-        self.D_A = Discriminator(self.opt)  # todo: args
-        self.D_B = Discriminator(self.opt)  # todo: args
-        self.criterion = None       # MSE? BCE? + cycle consistency loss, todo: can we make class of loss?
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.G_A = Generator(self.opt)
+        self.G_B = Generator(self.opt)
+
+        if is_train:
+        	# Only need discriminators at training time
+	        self.D_A = Discriminator(self.opt)
+	        self.D_B = Discriminator(self.opt)
+	        # Only need losses as training time
+        	self.criterionLSGAN = LSGANLoss().to(self.device)
+        	self.criterionCycle = nn.L1Loss()	# Cycle-consistency loss, see paper!
+        	# TODO: might want to test out identity loss as well.
+
+        	self.optimizer_G = optim.Adam(itertools.chain(self.G_A.parameters(), self.G_B.parameters()),
+        								  lr=self.opt.lr, betas=(self.opt.beta1, 0.999))
+        	self.optimizer_D = optim.Adam(itertools.chain(self.D_A.parameters(), self.D_B.parameters()),
+        								  lr=self.opt.lr, betas=(self.opt.beta1, 0.999))
 
     def train(self):
         pass
@@ -132,7 +190,7 @@ class CycleGAN:
     def test(self):
         pass
 
-    def forward(self, input):
+    def forward(self, x):
         # call forward methods in both generators and discriminators
         pass
 
@@ -142,8 +200,7 @@ class CycleGAN:
 if __name__ == "__main__":
     print("This program is still in the making.")
     print("Instantiating CycleGAN clone... ")
-    cycle_gan = CycleGAN()
+    cycle_gan = CycleGAN(is_train=True)
     print(cycle_gan.opt)
-    print(cycle_gan.G_A)
-    print(cycle_gan.G_B)
-    
+    print(cycle_gan.D_A)
+    print(cycle_gan.D_B)
